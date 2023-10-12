b0a30690be2808ba460dfd2cf3a21195293ecfa5
steve-leo254 <steveleo254@gmail.com>
2023-10-12 15:06:18 +0300

    sql

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def hello():
     new_user=User(name="stevoh",email="steveleo254@gmail.com")
     db.session.add(new_user)
     db.session.commit()
-    new_user2= User(name="john",email="john2544@gmail.com")
-    db.session.add(new_user2)
-    db.session.commit()
+    # new_user2= User(name="john",email="john2544@gmail.com")
+    # db.session.add(new_user2)
+    # db.session.commit()
     return "hi"
 
 app.run()
